1ff4b7a7a725774846b1efffa1a810fd0e95bcef
FlorentTauty <florent.tauty@epita.fr>
2022-09-30 19:28:29 +0200

    [ADD][ROUTES] / redirect to LoginPage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -17,9 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(
-        title: "Login",
-      ),
+      routes: {'/': (context) => const LoginPage(title: "Login")},
     );
   }
 }
