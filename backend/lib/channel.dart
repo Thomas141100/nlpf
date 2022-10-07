8663aa1bc8545b1e2de68dafdfa9c6e6e51ced49
Thomas <thomas.saury@epita.fr>
2022-10-07 18:02:39 +0200

    Added joboffer candidacy controller

--- backend/lib/channel.dart
+++ backend/lib/channel.dart
@@ -3,6 +3,7 @@ import 'package:conduit/conduit.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 import 'controller/auth_controller.dart';
+import 'controller/joboffer_candidacy_controller.dart';
 import 'controller/joboffer_controller.dart';
 import 'controller/user_controller.dart';
 
@@ -20,12 +21,11 @@ class Channel extends ApplicationChannel {
   @override
   Controller get entryPoint {
     final router = Router();
-    // router.route("/auth/signup").linkFunction((request) => signup(request, db));
-    // router.route("/auth/login").linkFunction((request) => login(request, db));
     router.route("/auth/[:action]").link(() => MyAuthController(db));
 
     router.route("/users/[:id]").link(() => UserController(db));
     router.route("/joboffers/[:id]").link(() => JobOfferController(db));
+    router.route("/joboffers/:offerId/candidacies").link(() => JobOfferCandidacyController(db));
 
     return router;
   }
---
+++ backend/lib/controller/joboffer_candidacy_controller.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:backend/helper/authorized.dart';
+import 'package:conduit/conduit.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class JobOfferCandidacyController extends ResourceController {
+  JobOfferCandidacyController(this.db);
+
+  final Db db;
+
+  @override
+  FutureOr<RequestOrResponse> willProcessRequest(Request req) async {
+    if (req.raw.headers['authorization']?[0] == null)
+      return Response.unauthorized();
+
+    // check if the user is authorized
+    if (!await isAuthorized(db, req.raw.headers['authorization']![0]))
+      return Response.forbidden();
+
+    return req;
+  }
+
+  @Operation.get('offerId')
+  Future<Response> getAllCandidacies(
+      @Bind.path('offerId') String offerId) async {
+    final collection = db.collection("candidacies");
+
+    var query = where.eq("offer", ObjectId.fromHexString(offerId));
+
+    final result = await collection.find(query).toList();
+
+    for (final candidacy in result) {
+      final userCollection = db.collection("users");
+      final user = await userCollection.findOne(where
+          .excludeFields(["password", "candidacies"])
+          .eq("_id", ObjectId.fromHexString(candidacy["candidate"] as String)));
+      candidacy["candidate"] = user;
+    }
+    return Response.ok(result);
+  }
+
+  @Operation.post('offerId')
+  Future<Response> addCandidacy(@Bind.path('offerId') String offerId) async {
+    if (request?.body == null || request!.body.isEmpty) {
+      return Response.badRequest(body: {"error": "No body"});
+    }
+
+    final user = getPayload(request!.raw.headers['authorization']![0]);
+    if (user['isCompany'] == true) {
+      return Response.forbidden();
+    }
+
+    final Map<String, dynamic> candidate = await request!.body.decode();
+    candidate['creationDate'] = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+
+    if (!candidate.containsKey('candidate')) {
+      candidate['candidate'] = user['id'];
+    }
+    if (!candidate.containsKey('offer')) {
+      candidate['offer'] = ObjectId.fromHexString(offerId);
+    }
+
+    final candidacyCollection = db.collection("candidacies");
+    final inserted = await candidacyCollection.insertOne(candidate);
+
+    final jobOfferCollection = db.collection("joboffers");
+    final jobOfferResult = await jobOfferCollection.updateOne(
+        where.eq("_id", ObjectId.fromHexString(offerId)),
+        modify.addToSet("candidacies", inserted.document!['_id']));
+
+    final userCollection = db.collection("users");
+    final userResult = await userCollection.updateOne(
+        where.eq("_id", ObjectId.fromHexString(user['id'] as String)),
+        modify.addToSet("candidacies", inserted.document!['_id']));
+
+    return Response.ok(inserted.document);
+  }
+}
