c27484e7a97ddf7b5accf7f770780e795441e436
FlorentTauty <florent.tauty@epita.fr>
2022-10-08 15:30:07 +0200

    [RENAME] job_offer to respect Dart notation

--- frontend/lib/models/joboffer.dart
+++
@@ -1,9 +0,0 @@
-class JobOffer {
-  String _id = "";
-  String title = "";
-  String employer = "";
-  String companyName = "";
-  List<String>? candidacies;
-  List<String>? tags;
-  String? description;
-}
