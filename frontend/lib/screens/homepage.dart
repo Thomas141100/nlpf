b777e174612d97d78ccdc9b982b3a69f50733c73
FlorentTauty <florent.tauty@epita.fr>
2022-10-08 12:32:26 +0200

    [FIX] create offer only for company user

--- frontend/lib/screens/homepage.dart
+++ frontend/lib/screens/homepage.dart
@@ -72,77 +72,79 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            barrierDismissible: false,
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                content: CreateJobOffer(
-                  titleController: titleController,
-                  descriptionController: descriptionController,
-                  tagsController: tagsController,
-                  companyNameController: companyNameController,
-                  formKey: _jobOfferformKey,
-                ),
-                actions: [
-                  TextButton(
-                    style:
-                        TextButton.styleFrom(foregroundColor: Colors.redAccent),
-                    onPressed: () {
-                      titleController.clear();
-                      descriptionController.clear();
-                      tagsController.clear();
-                      companyNameController.clear();
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancel'),
-                  ),
+      floatingActionButton: _currentUser != null && _currentUser!.isCompany
+          ? FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: CreateJobOffer(
+                        titleController: titleController,
+                        descriptionController: descriptionController,
+                        tagsController: tagsController,
+                        companyNameController: companyNameController,
+                        formKey: _jobOfferformKey,
+                      ),
+                      actions: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                              foregroundColor: Colors.redAccent),
+                          onPressed: () {
+                            titleController.clear();
+                            descriptionController.clear();
+                            tagsController.clear();
+                            companyNameController.clear();
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Cancel'),
+                        ),
 
-                  // The "Yes" button
-                  TextButton(
-                    onPressed: () async {
-                      if (_jobOfferformKey.currentState!.validate()) {
-                        // Client
-                        var response = await Client.sendJobOffer(
-                          titleController.text,
-                          descriptionController.text,
-                          tagsController.text,
-                          companyNameController.text,
-                        );
-                        if (response.statusCode == 200) {
-                          Navigator.of(context).pop();
-                          showSnackBar(context, "JobOffer Created");
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return const AlertDialog(
-                                // Retrieve the text the that user has entered by using the
-                                // TextEditingController.
-                                content: Text("JobOffer Creation Failed"),
+                        // The "Yes" button
+                        TextButton(
+                          onPressed: () async {
+                            if (_jobOfferformKey.currentState!.validate()) {
+                              // Client
+                              var response = await Client.sendJobOffer(
+                                titleController.text,
+                                descriptionController.text,
+                                tagsController.text,
+                                companyNameController.text,
                               );
-                            },
-                          );
-                        }
-                      }
+                              if (response.statusCode == 200) {
+                                Navigator.of(context).pop();
+                                showSnackBar(context, "JobOffer Created");
+                              } else {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return const AlertDialog(
+                                      // Retrieve the text the that user has entered by using the
+                                      // TextEditingController.
+                                      content: Text("JobOffer Creation Failed"),
+                                    );
+                                  },
+                                );
+                              }
+                            }
 
-                      titleController.clear();
-                      descriptionController.clear();
-                      tagsController.clear();
-                      companyNameController.clear();
-                    },
-                    child: const Text('Post'),
-                  ),
-                ],
-              );
-            },
-          );
-        },
-        tooltip: 'Post a JobOffer',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+                            titleController.clear();
+                            descriptionController.clear();
+                            tagsController.clear();
+                            companyNameController.clear();
+                          },
+                          child: const Text('Post'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              tooltip: 'Post a JobOffer',
+              child: const Icon(Icons.add),
+            )
+          : null, // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
