8f45f50ebdbc0cd400191c34a94ffc37fc2f3f43
Bertrand <bertrand.tarascon@epita.fr>
2022-10-08 14:35:25 +0200

    [ADD][QCM] QCM form working in standalone mode

---
+++ frontend/lib/forms/quiz.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  const Quiz({
+    Key? key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Question(
+            questions[questionIndex]['questionText'].toString(),
+          ),
+          ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+              .map((answer) {
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Answer(
+                () => answerQuestion(answer['score']),
+                answer['text'].toString(),
+              ),
+            );
+          }).toList()
+        ],
+      ),
+    );
+  }
+}
