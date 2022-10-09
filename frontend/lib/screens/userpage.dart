a9968ac76ca3f7a26a981470aa2f106e6f6f4fe7
Axelle COURIOL <axelle.couriol@epita.fr>
2022-10-09 13:02:30 +0200

    [WIP] add field to modify profil, controller and onPressed missing

--- frontend/lib/screens/userpage.dart
+++ frontend/lib/screens/userpage.dart
@@ -45,7 +45,7 @@ class _UserPageState extends State<UserPage> {
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
             title: const Text('Selectionnez un fichier multimedia'),
             content: SizedBox(
-              height: MediaQuery.of(context).size.height / 6,
+              height: MediaQuery.of(context).size.height / 13,
               child: Column(
                 children: [
                   ElevatedButton(
@@ -57,9 +57,12 @@ class _UserPageState extends State<UserPage> {
                     child: Row(
                       children: const [
                         Icon(Icons.image),
-                        Text('Depuis la galerie de photos'),
+                        Text(' Depuis la galerie de photos'),
                       ],
                     ),
+                  ),
+                  const SizedBox(
+                    height: 10,
                   ),
                   ElevatedButton(
                     //if user click this button. user can upload image from camera
@@ -70,7 +73,7 @@ class _UserPageState extends State<UserPage> {
                     child: Row(
                       children: const [
                         Icon(Icons.camera),
-                        Text('Prendre une photo'),
+                        Text(' Prendre une photo'),
                       ],
                     ),
                   ),
@@ -92,6 +95,9 @@ class _UserPageState extends State<UserPage> {
     if (_currentUser == null) {
       setCurrentUser();
     }
+    String _firstname = _currentUser != null ? _currentUser!.firstname : 'NC';
+    String _lastname = _currentUser != null ? _currentUser!.lastname : 'NC';
+    String _email = _currentUser != null ? _currentUser!.email : 'NC';
     return Scaffold(
       appBar: Header(
         key: const ValueKey('header'),
@@ -104,161 +110,221 @@ class _UserPageState extends State<UserPage> {
           mainAxisSize: MainAxisSize.max,
           children: [
             Expanded(
-                child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      myAlert();
-                    },
-                    child: const Icon(Icons.photo_camera)),
-                const SizedBox(
-                  height: 10,
-                ),
-                //if image not null show the image
-                //if image null show text
-                image != null
-                    ? Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            //to show image, you type like this.
-                            image!.path,
-                            fit: BoxFit.cover,
-                            width: 300,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        myAlert();
+                      },
+                      child: const Icon(Icons.photo_camera)),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  //if image not null show the image
+                  //if image null show text
+                  image != null
+                      ? Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              //to show image, you type like this.
+                              image!.path,
+                              fit: BoxFit.cover,
+                              width: 300,
+                              height: 300,
+                            ),
+                          ))
+                      : Column(children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
                             height: 300,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(67, 96, 125, 139),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Center(
+                                child: Text(
+                              "Aucune photo de profil",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            )),
                           ),
-                        ),
-                      )
-                    : const Text(
-                        "Actuellement aucune photo de profil",
-                        style: TextStyle(fontSize: 20),
-                      )
-              ],
-            )),
+                        ]),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 180,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          myAlert();
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(Icons.upload_file),
+                            Text(
+                              'Choisir un fichier',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  Row(
-                    children: [
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 100,
-                              child: const Text(
-                                'Nom : ',
-                                style: TextStyle(fontSize: 20),
-                                textAlign: TextAlign.right,
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: SizedBox(
+                              width: 250,
+                              child: TextFormField(
+                                //controller:
+                                decoration: InputDecoration(
+                                    border: const OutlineInputBorder(),
+                                    labelText: _firstname),
                               ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 100,
-                              child: const Text(
-                                'Prenom : ',
-                                style: TextStyle(fontSize: 20),
-                                textAlign: TextAlign.right,
+                            )),
+                        Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: SizedBox(
+                              width: 250,
+                              child: TextFormField(
+                                //controller:
+                                decoration: InputDecoration(
+                                    border: const OutlineInputBorder(),
+                                    labelText: _lastname),
                               ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 100,
-                              child: const Text(
-                                'Mail : ',
-                                style: TextStyle(fontSize: 20),
-                                textAlign: TextAlign.right,
+                            )),
+                        Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: SizedBox(
+                              width: 250,
+                              child: TextFormField(
+                                //controller:
+                                decoration: InputDecoration(
+                                    border: const OutlineInputBorder(),
+                                    labelText: _email),
                               ),
-                            )
-                          ]),
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black)),
-                              child: Text(
-                                _currentUser != null
-                                    ? _currentUser!.firstname
-                                    : 'NC',
-                                style: const TextStyle(fontSize: 20),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black)),
-                              child: Text(
-                                _currentUser != null
-                                    ? _currentUser!.lastname
-                                    : 'NC',
-                                style: const TextStyle(fontSize: 20),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(15.0),
-                              padding: const EdgeInsets.all(3.0),
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black)),
-                              child: Text(
-                                _currentUser != null
-                                    ? _currentUser!.email
-                                    : 'NC',
-                                style: const TextStyle(fontSize: 20),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ]),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.red),
-                    child: Column(
-                      children: const [
-                        Icon(Icons.remove_circle, color: Colors.white),
-                        Text(
-                          'Supprimer le compte',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
+                            )),
+                      ]),
+                  SizedBox(
+                    width: 200,
+                    child: Column(children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 10, 129, 12),
                         ),
-                      ],
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: ((context) => const ConfirmationDialog(
-                                key:
-                                    ValueKey('confirmation_dialog_user_delete'),
-                                title: "Confirmation Dialog",
-                                message:
-                                    "Êtes vous sûr de vouloir supprimer cet utilisateur ?",
-                              )));
-                    },
+                        onPressed: () {
+                          //FIXME:
+                        },
+                        child: Row(children: const [
+                          Icon(Icons.save, color: Colors.white),
+                          Text(
+                            ' Valider le changement',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ]),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        child: Row(children: const [
+                          Icon(Icons.remove_circle, color: Colors.white),
+                          Text(
+                            ' Supprimer le compte',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ]),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: ((context) => const ConfirmationDialog(
+                                    key: ValueKey(
+                                        'confirmation_dialog_user_delete'),
+                                    title: "Confirmation Dialog",
+                                    message:
+                                        "Êtes vous sûr de vouloir supprimer cet utilisateur ?",
+                                  )));
+                        },
+                      ),
+                    ]),
                   ),
                 ],
               ),
-            )
+            ),
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(15.0),
+                  width: 250,
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Nouveau mot de passe')),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(15.0),
+                  width: 250,
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Confirmer le mot de passe')),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 10, 129, 12),
+                    ),
+                    onPressed: () {
+                      //FIXME:
+                    },
+                    child: Row(children: const [
+                      Icon(Icons.save, color: Colors.white),
+                      Text(
+                        'Valider le changement',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ]),
+                  ),
+                )
+              ],
+            ))
           ],
         ),
       ),
