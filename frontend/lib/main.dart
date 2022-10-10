9a54bf07d8ec302693f33e2804ebbb8a75618280
Axelle COURIOL <axelle.couriol@epita.fr>
2022-10-10 20:07:41 +0200

    [ADD] add colors for other objects

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -48,50 +48,53 @@ class MyAppState extends State<MyApp> {
           secondaryHeaderColor: const Color.fromARGB(255, 36, 58, 115),
           backgroundColor: const Color.fromARGB(255, 242, 235, 233),
           fontFamily: 'Baskervville', //GoogleFonts.baskervville(),
-          textTheme: TextTheme(
+          hoverColor: const Color.fromARGB(255, 124, 62, 102),
+          cardColor: const Color.fromARGB(255, 242, 235, 233),
+          highlightColor: const Color.fromARGB(255, 124, 62, 102),
+          textTheme: const TextTheme(
             displayLarge: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 35,
-                          color: const Color.fromARGB(255, 36, 58, 115)),
+                          color: Color.fromARGB(255, 36, 58, 115)),
             displayMedium: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 30,
-                          color: const Color.fromARGB(255, 36, 58, 115)),
+                          color: Color.fromARGB(255, 36, 58, 115)),
           titleLarge: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 35,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
           titleMedium: TextStyle(
                           fontSize: 20.0,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
           titleSmall: TextStyle(
                           fontSize: 15.0,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
 
           bodyLarge: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.w500,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
           bodyMedium: TextStyle(
                           fontSize: 12,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
                         
           bodySmall: TextStyle(
                           fontSize: 10,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
           labelLarge: TextStyle(
                           fontSize: 20.0,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
           labelMedium: TextStyle(
                           fontSize: 15.0,
-                          color: const Color.fromARGB(255, 36, 58, 115),
+                          color: Color.fromARGB(255, 36, 58, 115),
                         ),
                         labelSmall:  TextStyle(
                           fontSize: 15,
