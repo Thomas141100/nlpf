792bd5eb5160fe3ffb357d36e8dfc4470fb36bb3
Axelle COURIOL <axelle.couriol@epita.fr>
2022-10-10 16:55:12 +0200

    [ADD] add theme and specified text in login page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:fht_linkedin/routes/router.gr.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -41,6 +42,60 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: const Color.fromARGB(255, 124, 62, 102),
+          secondaryHeaderColor: const Color.fromARGB(255, 36, 58, 115),
+          backgroundColor: const Color.fromARGB(255, 242, 235, 233),
+          fontFamily: 'Baskervville', //GoogleFonts.baskervville(),
+          textTheme: TextTheme(
+            displayLarge: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 35,
+                          color: const Color.fromARGB(255, 36, 58, 115)),
+            displayMedium: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30,
+                          color: const Color.fromARGB(255, 36, 58, 115)),
+          titleLarge: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 35,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+          titleMedium: TextStyle(
+                          fontSize: 20.0,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+          titleSmall: TextStyle(
+                          fontSize: 15.0,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+
+          bodyLarge: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+          bodyMedium: TextStyle(
+                          fontSize: 12,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+                        
+          bodySmall: TextStyle(
+                          fontSize: 10,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+          labelLarge: TextStyle(
+                          fontSize: 20.0,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        ),
+          labelMedium: TextStyle(
+                          fontSize: 15.0,
+                          color: const Color.fromARGB(255, 36, 58, 115),
+                        )
+              
+              ),
+        ),
         routeInformationParser: _appRouter.defaultRouteParser(),
         routerDelegate: _appRouter.delegate());
   }
